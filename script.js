6006354a03b399cfe4d2e7a59e6643d3d63ab34d
MattRuetz <matt.b.ruetz@gmail.com>
2022-02-23 11:57:02 -0500

    three user-activated style options for entire page color scheme (pattern, gradient, or image).

---
+++ script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('.wrapper');
+
+const changeBackground = (number) => {
+    switch (number) {
+        case '1':
+            body.classList.add('background-1');
+            body.classList.remove('background-2');
+            body.classList.remove('background-3');
+            break;
+        case '2':
+            body.classList.add('background-2');
+            body.classList.remove('background-1');
+            body.classList.remove('background-3');
+            break;
+        case '3':
+            body.classList.add('background-3');
+            body.classList.remove('background-2');
+            body.classList.remove('background-1');
+            break;
+        default:
+            break;
+    }
+};
